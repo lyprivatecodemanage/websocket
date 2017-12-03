085f9f76348956ae804c4eed7e6187fd5c38c569
李业 <liye@xiangshangban.com>
2017-12-03 18:23:12 +0800

    first_commit

---
+++ src/main/java/com/jinnian/websoket/App.java
@@ -0,0 +1,19 @@
+package com.jinnian.websoket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
